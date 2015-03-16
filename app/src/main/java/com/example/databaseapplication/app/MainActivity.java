d0044aeea9d3ad40d8968a2a340943ecca67e8ce
24digital <24digital@gmail.com>
2015-03-16 14:37:51 -0400

    Adding functionality .

--- app/src/main/java/com/example/databaseapplication/app/MainActivity.java
+++ app/src/main/java/com/example/databaseapplication/app/MainActivity.java
@@ -4,16 +4,46 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
-
+//List view to hold entities
+    private ListView listView;
+    private ListAdapter adapter;
+    private ArrayList list;
+    private EmergencyContact contact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        if(listView == null)
+        {
+            listView = (ListView)findViewById(R.id.listView);
+            adapter = new ArrayAdapter<String>(this, R.layout.activity_main);
+            listView.setAdapter(adapter);
+            list = new ArrayList();
+        }
+
+
+
+
+}
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    listView = (ListView)    savedInstanceState.get("list");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
