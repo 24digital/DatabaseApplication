8787d16e17684e41dc9387c144512811b56ddefb
24digital <24digital@gmail.com>
2015-03-19 09:18:51 -0400

    Adding functionality .

--- app/src/main/java/com/example/databaseapplication/app/MainActivity.java
+++ app/src/main/java/com/example/databaseapplication/app/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.databaseapplication.app;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
+
 import android.app.ListActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -115,24 +113,6 @@ public class MainActivity extends ListActivity
         super.onPause();
     }
 
-    public static class FireMissilesDialogFragment extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the Builder class for convenient dialog construction
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(R.string.dialog_fire_missiles)
-                    .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // FIRE ZE MISSILES!
-                        }
-                    })
-                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // User cancelled the dialog
-                        }
-                    });
-            // Create the AlertDialog object and return it
-            return builder.create();
-        }
-    }
+
+
 }
